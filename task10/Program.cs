a51d71fb0eba845aba4e059b536b095e1e2e2c60
VGEEK BRANCH <vgeekbranch@MacBook-Pro-VGEEK.local>
2022-10-06 15:10:00 +0300

    task10HomeWrork

---
+++ task10/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 10: Напишите программу, 
+которая принимает на вход трёхзначное число
+ и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int number = new Random().Next(100, 1000); // генерируем случайное трехзначное число
+
+int getCreateTwoDigitFromNumber(int number) // функция принимает на вход трёхзначное число и на выходе показывает вторую цифру mid = (n % 100) / 10
+{
+    int midleNumber = (number % 100) / 10;
+    return(midleNumber);
+}
+
+Console.Write($"{number} -> ");
+int result = getCreateTwoDigitFromNumber(number);
+Console.WriteLine(result);
