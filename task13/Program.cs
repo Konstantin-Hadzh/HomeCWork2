02f079e9aef3a2711657906d90f37ccf9a8a1631
VGEEK BRANCH <vgeekbranch@MacBook-Pro-VGEEK.local>
2022-10-06 15:13:59 +0300

    task13_HomeWork

---
+++ task13/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа
+или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+int thirdNumber = new Random().Next(1, 999);
+string thirdNumberText = Convert.ToString(thirdNumber);
+if (thirdNumberText.Length > 2)
+{
+    Console.Write($"{thirdNumberText} третья цифра -> {thirdNumberText[2]}");
+    Console.WriteLine();
+}
+else 
+{
+    Console.WriteLine($"{thirdNumberText} -> третьей цифры нет");
+}
